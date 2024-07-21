219ba9581d6a0c1bb89b039afc41e30ac72272a7
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-07-21 12:10:59 +0200

    adding documentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,6 +32,9 @@ class Server:
         return self.__dataset
     
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return the appropriate page of the dataset
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
